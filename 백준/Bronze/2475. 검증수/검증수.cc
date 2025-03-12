b94262c828f2991f97e1a3ae7b30c9009b1ae1a6
BaekRaven <58510698+BaekRaven@users.noreply.github.com>
2025-03-12 14:51:15 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num[6];
+    for(int i=0; i<5; i++){        
+        cin >> num[i];
+    }
+
+    int result = 0;
+    for(int i=0; i<5; i++){        
+        result += num[i]*num[i];
+    }
+    
+    cout << result%10;    
+}
