1f2396bf4e2d96ee732673d2114b19dc5c69e8de
BaekRaven <58510698+BaekRaven@users.noreply.github.com>
2025-03-13 13:27:57 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int grade;
+    cin >> grade;
+
+    if(90<=grade && grade<=100){
+        cout << 'A';
+    }
+    else if(80<=grade && grade<=89){
+        cout << 'B';
+    }
+    else if(70<=grade && grade<=79){
+        cout << 'C';
+    }
+    else if(60<=grade && grade<=69){
+        cout << 'D';
+    }
+    else{
+        cout << 'F';
+    }
+}
